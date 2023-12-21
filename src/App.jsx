d027e6f14f7b62b545d07ad152d70a8060ca1950
Shourya Gupta <75240318+shourya2002-geek@users.noreply.github.com>
2023-12-21 16:25:20 +0530

    Added comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+// Import the React library, which is required for creating React components.
 import React from "react";
+
+// Import the CSS file for styling this component.
 import "./App.css";
+
+// Import the TaskDashboard component from the specified path.
 import TaskDashboard from "./Context/TaskDashboard";
+
+// Define the main App component.
 function App() {
+  // Render the App component's content.
   return (
     <>
+      {/* Render the TaskDashboard component within a fragment. */}
       <>
         <TaskDashboard />
       </>
@@ -11,4 +20,5 @@ function App() {
   );
 }
 
+// Export the App component as the default export of this module.
 export default App;
